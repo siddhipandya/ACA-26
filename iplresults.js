05796a0a85beffd1a9f513f74df81919831c8884
siddhipandya <82381765+siddhipandya@users.noreply.github.com>
2021-06-03 16:13:50 +0530

    Add files via upload

---
+++ iplresults.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCV465KnMhJaFcOxrv3un2A2Y0bJqjesUc",
+    authDomain: "sports-website-8d572.firebaseapp.com",
+    databaseURL: "https://sports-website-8d572-default-rtdb.firebaseio.com",
+    projectId: "sports-website-8d572",
+    storageBucket: "sports-website-8d572.appspot.com",
+    messagingSenderId: "298397552777",
+    appId: "1:298397552777:web:8c6271872e8e0bc1ac3658"
+  };
